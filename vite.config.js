29549bb6d9daa1a42c3615f18e815d3a227141aa
rbgamedev1 <rbgamedev1@gmail.com>
2025-11-05 18:15:29 -0300

    b

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'giovanny-plantable-olen.ngrok-free.dev'
+  ],
+    host: true,
+  },
 })
